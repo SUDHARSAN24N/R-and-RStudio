737ae9a8d3fe1789290a14a06785627df5608344
N.Sudharsan <148472454+SUDHARSAN24N@users.noreply.github.com>
2025-08-21 00:06:27 -0700

    program 8 is apriori alg
    dot and varianble understand easily to be declaring

--- pg8.R
+++ pg8.R
@@ -6,15 +6,6 @@ transactions <- list(
   c("bread","butter","jam"),
   c("milk","bread","butter","jam")
 )
-min_support<-0.3
-min_conf<-0.7transactions <- list(
-  c("milk","bread","butter"),
-  c("bread","butter"),
-  c("milk","bread"),
-  c("milk","butter"),
-  c("bread","butter","jam"),
-  c("milk","bread","butter","jam")
-)
 
 #thresholds
 min_support<-0.3
@@ -50,30 +41,4 @@ for(i in 1:(length(items)-1))
      if(sAB>=min_support && cB_A>=min_conf)
       cat(sprintf("{%s}=>{%s}(support:%.2f,conf:%.2f)\n",B,A,sAB,cB_A))
   }
-}
-support<- function (item,trans){
-  sum(supply(trans,functions(t)all(items%\n%\t))),
-  length(trans)
-}
-items<-unique(unlist(transactions))
-cat("frequent 1-itemsets:\n")
-for(i in items)
-{
-  s<-support(i,transactions)
-  if(s>=min_support)
-    cat(printf("{%s}support:%2f\n",i,s))
-}
-cat("\n Association Rules:\n")
-for(i in 1:(length(items)-1))
-{
-  for(j in (i+1):length(items))
-  {
-    A<- items[1];B<-items[j]
-    AB<-support((A,B),transactions)
-    A-B<-SAB/support(A,transactions)
-    if(SAB>=min_support&&(A-B>=min_conf)
-       cat(sprintf("{%s}=>{%s}(support:%2f,con:%2f)\n",A,B,SAB,(A-B))))
-    
-  }
-
 }
